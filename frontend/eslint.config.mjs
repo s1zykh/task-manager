892d77b008e99cab4ecdb013cc43dab6a8c1e9bd
s1zykh <vladik.sizykh21@mail.ru>
2025-06-30 00:41:28 +0300

    install dependencies and configure TanStack Query

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,16 +1,24 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+	...compat.config({
+		extends: [
+			'next/core-web-vitals',
+			'next/typescript',
+			'prettier',
+			'plugin:@tanstack/eslint-plugin-query/recommended'
+		],
+		plugins: ['@tanstack/query']
+	})
+]
 
-export default eslintConfig;
+export default eslintConfig
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+env: {
+  SERVER_URL: process.env.SERVER_URL
+},
+inages: {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'lh3.googleusercontent.com',
+    }, {
+      protocol: 'https',
+      hostname: 'avatars.yandex.net',
+    }
+  ]
+}
 };
 
 export default nextConfig;
